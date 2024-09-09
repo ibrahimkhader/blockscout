2b1e1325f2f69ac1e6b269c46e0c79320892f573
ibrahim <i.khader@tigercrypto.uk>
2024-09-09 11:45:08 +0500

    updated

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 version: 0.0
 os: linux
 files:
-- source: /
+  - source: /
     destination: /var/app
 file_exists_behavior: OVERWRITE
 permissions:
@@ -11,17 +11,17 @@ permissions:
     group: ubuntu
 
 hooks:
-ApplicationStop:
-    - location: scripts/stop.sh
-      timeout: 300
-      runas: root
+  ApplicationStop:
+      - location: scripts/stop.sh
+        timeout: 300
+        runas: root
 
-AfterInstall:
-    - location: scripts/pre_start.sh
-      timeout: 600
-      runas: root
+  AfterInstall:
+      - location: scripts/pre_start.sh
+        timeout: 600
+        runas: root
 
-ApplicationStart:
-    - location: scripts/start.sh
-      timeout: 600
-      runas: root
+  ApplicationStart:
+      - location: scripts/start.sh
+        timeout: 600
+        runas: root
